0e366716082d4ced306cf2a42cbcd70cd72a50cf
Antoine Place <mail.place.a@gmail.com>
2025-12-14 02:23:25 +0100

    feat: added openapi specification for alchemy backend

--- src/main/java/alchemy/Application.java
+++ src/main/java/alchemy/Application.java
@@ -2,10 +2,6 @@ package alchemy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.event.ContextRefreshedEvent;
-import org.springframework.context.event.EventListener;
-import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 @SpringBootApplication
 public class Application {
@@ -13,12 +9,5 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-	
-	@EventListener
-    public void handleContextRefresh(ContextRefreshedEvent event) {
-        ApplicationContext applicationContext = event.getApplicationContext();
-        applicationContext.getBean(RequestMappingHandlerMapping.class)
-            .getHandlerMethods().forEach((key, value) -> System.out.println(String.format("%s %s", key, value)));
-    }
 
 }
